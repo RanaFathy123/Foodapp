90e3f082ad5c02e5bb26faec2223f733c945256a
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-04-23 01:47:20 +0200

    Logout function

--- src/modules/SharedModule/components/SideBar/SideBar.jsx
+++ src/modules/SharedModule/components/SideBar/SideBar.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function SideBar() {
+  const navigate = useNavigate()
+  const logOut = ()=>{
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
   return (
-    <div>SideBar</div>
+    <div>
+      <div>Sidebar</div>
+      <button onClick={logOut} className='btn btn-danger'>LogOut</button>
+    </div>
   )
 }
