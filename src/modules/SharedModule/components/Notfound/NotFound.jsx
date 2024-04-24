8bef5dbd2e3c6b61c26e96e8dd21818fe0c85f72
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-04-24 23:46:04 +0200

    change in page to be more responsive

--- src/modules/SharedModule/components/Notfound/NotFound.jsx
+++ src/modules/SharedModule/components/Notfound/NotFound.jsx
@@ -9,31 +9,24 @@ export default function NotFound() {
     navigate("/");
   };
   return (
-    <div className="container-fluid notfound-bg vh-100 p-3">
+    <div className="container-fluid">
       <img src={logo} className="m-2" />
-      <div className="container-fluid">
-        <div className="row  align-items-center w-100 p-5">
-          <div className="col-md-7">
+      <div className="container-fluid notfound-bg vh-100">
+        <div className="row align-items-center justify-content-center vh-100">
+          <div className="col-md-6 col-sm-10 ">
             <h1 className="mb-3">Oops.</h1>
             <h3 className="text-success mb-3">Page not found</h3>
-            <p
-              className="mb-4"
-              style={{ height: "39.61px", width: "302.36px" }}
-            >
+            <p className="mb-4"  style={{ height: "39.61px", width: "302.36px" }}>
               This Page doesn’t exist or was removed! We suggest you back to
               home
             </p>
-            <button
-              className="btn btn-success "
-              style={{ height: "60.68px", width: "302.36px" }}
-              onClick={gotToHome}
-            >
+            <button className="btn btn-success" onClick={gotToHome}>
               <i className="fa fa-arrow-left m-3" aria-hidden="true" />
               Back To Home
             </button>
           </div>
-          <div className="col-md-2">
-            <img src={notfound} className="m-3 " />
+          <div className="col-md-4 col-sm-10 text-center">
+            <img src={notfound} className="m-3 img-fluid" />
           </div>
         </div>
       </div>
