b0435d388ca6449a83b73a9adc1cb660da4da6c9
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-04-29 16:03:13 +0300

    No Data Component

---
+++ src/modules/SharedModule/components/NoData/NoData.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import noDataImg from "../../../../assets/images/no-data.png";
+export default function NoData() {
+  return (
+    <div className="d-flex flex-column gap-2 justify-content-center  align-items-center ">
+      <img src={noDataImg} alt="" />
+      <h3>NoData</h3>
+      <p className="text-center">are you sure you want to delete this item ? if you are sure just click on delete it</p>
+    </div>
+  );
+}
