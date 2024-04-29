2cc919e4ff0b93f2e8b102a385685bd7683932f8
RanaFathy123 <134057137+RanaFathy123@users.noreply.github.com>
2024-04-29 16:17:05 +0300

    enhance nodata design

--- src/modules/SharedModule/components/NoData/NoData.jsx
+++ src/modules/SharedModule/components/NoData/NoData.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import noDataImg from "../../../../assets/images/no-data.png";
 export default function NoData() {
   return (
-    <div className="d-flex flex-column gap-2 justify-content-center  align-items-center ">
+    <div className="d-flex flex-column gap-2 justify-content-center  align-items-center mt-4">
       <img src={noDataImg} alt="" />
       <h3>NoData</h3>
       <p className="text-center">are you sure you want to delete this item ? if you are sure just click on delete it</p>
